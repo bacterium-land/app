59d42d1c408310a362c31151186adfbcbc54b5e4
jarpi <me@jarpi>
2020-10-03 19:40:57 +0200

    backend: Create game #15

--- bacterialand-server/src/js/bacterium-be/game/index.js
+++ bacterialand-server/src/js/bacterium-be/game/index.js
@@ -7,18 +7,32 @@ const url = process.env.URI_DB || ''
 
 // Create a new MongoClient
 const client = new MongoClient(url, { useNewUrlParser: true  });
-const mongoConnect = util.promisify(client.connect).bind(client);
 let mongoConnection = null;
 
 module.exports = async function (context, req) {
+    console.dir(req)
     console.dir(context)
     context.log('JavaScript HTTP trigger function processed a request.');
     await connectToMongo()
-    const collection = await client.db('bacterium-store').collection('game');
     console.dir('connected to DB');
+    if (req.method === 'GET') {
+        await getGame(context, req);
+    }
+    else if (req.method === 'POST'){
+        await createGame(context, req);
+    }
+    else {
+        context.res = {
+            status: 404,
+            body: 'Invalid method'
+        };
+    }
+}
+
+async function getGame(context, req) {
+    const collection = await client.db('bacterium-store').collection('game');
     const gameId = req.query.gameId
     const game = await collection.find({_id: ObjectId(gameId)}).toArray()
-    // client.close();
     console.dir(`returning ${game.length} doc`);
     context.res = {
         // status: 200, /* Defaults to 200 */
@@ -26,7 +40,20 @@ module.exports = async function (context, req) {
     };
 }
 
+async function createGame(context, req) {
+    const collection = await client.db('bacterium-store').collection('game');
+    const insertResult = await collection.insertOne({
+        turn: req.body.player_id,
+        player1: [],
+        player2:[]
+    })
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: JSON.stringify({'game-id': insertResult.insertedId.toString()})
+    };
+}
+
 async function connectToMongo() {
-    if (!mongoConnection) mongoConnection = await mongoConnect()
+    if (!mongoConnection) mongoConnection = await client.connect()
     return mongoConnection
 }
