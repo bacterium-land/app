fb0f469fb080380baab86dcfd0aa4455e7aae326
jarpi <me@jarpi>
2020-10-04 12:48:24 +0200

    front: Get current game status and draw closes: #22

--- bacterialand-web/src/services/game.service.ts
+++ bacterialand-web/src/services/game.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +10,16 @@ export class GameService {
   url = 'https://bacterium-get-game.azurewebsites.net/api/game?code=AR0TI8kVnYzrILjYiBfStQy6oG0//zIcJk5Cd7oVdhMnt8a1nDUgWQ==';
   constructor(private http: HttpClient) { }
 
-  getGameById(): Observable<[]> {
-    const params = '&gameId=5f784e93ebafec6b9bec55f5';
+  getGameById(gameId: string): Observable<[]> {
+    const params = `&gameId=${gameId}`;
     return this.http.get<[]>(this.url + params);
   }
+
+  createNewGame(body: {}): Observable<{}> {
+    return this.http.post<{}>(this.url, body)
+    .pipe(
+      tap(val => console.dir(val))
+    )
+  }
+
 }
